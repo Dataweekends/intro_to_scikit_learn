17cada801169283aa31bf1379f67f2d1b54821c5
Francesco Mosconi <git@mosconi.me>
2016-02-17 16:53:13 -0800

    added advanced script

---
+++ advanced_starter_script.py
@@ -0,0 +1,109 @@
+
+# coding: utf-8
+__author__ = "Francesco Mosconi"
+__copyright__ = "Copyright 2016, Data Weekends"
+__license__ = "MIT"
+__email__ = "info@dataweekends.com"
+
+"""
+General purpose script detailing some of scikit-learn most common
+functions including:
+- data loading
+- several types of classifiers
+- preprocessing
+- pipelines
+- cross validation
+- grid search
+"""
+
+# Load a dataset from one of those available in sklearn:
+print "Simple starter scikit-learn example"
+print
+
+
+from sklearn.datasets import load_iris
+data = load_iris()
+# for other datasets see:
+# http://scikit-learn.org/stable/modules/classes.html#module-sklearn.datasets
+
+
+# Define features (X) and target (y) variables
+X = data['data']
+y = data['target']
+
+
+# Initialize a decision tree model
+from sklearn.tree import DecisionTreeClassifier
+estimator = DecisionTreeClassifier(max_depth=5, random_state=0)
+
+# other ideas for classifiers:
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.svm import SVC
+# from sklearn.naive_bayes import GaussianNB
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.ensemble import RandomForestClassifier
+
+
+# preprocess the data using a standard scaler
+from sklearn.preprocessing import StandardScaler
+transformer = StandardScaler()
+
+
+# combine the steps in a pipeline:
+# step1: transformer
+# step2: model
+# a pipeline can contain N transformer steps and an optional
+# final estimator step
+from sklearn.pipeline import make_pipeline
+pipeline = make_pipeline(transformer,
+                         estimator)
+
+
+# Do a cross validation:
+from sklearn.cross_validation import cross_val_score
+cvscores = cross_val_score(pipeline, X, y, n_jobs=-1)
+
+print "The pipeline CV score is:"
+print cvscores.mean().round(3), "+/-", cvscores.std().round(3)
+print
+
+# a pipeline exposes the API of a transformer and
+# estimator combined, so all the usual fit, predict and
+# transform methods are available.
+# if you're using text data or other data you could include
+# here preprocessing steps like:
+# - feature extraction
+# - feature selection
+# - normalization
+# - etc.
+
+# if your model has hyperparameters, try using grid search
+# to optimize them:
+from sklearn.grid_search import GridSearchCV
+from sklearn.svm import SVC
+model = SVC()
+params = {'kernel': ('linear', 'rbf'), 'C': [1, 10]}
+gsmodel = GridSearchCV(model, params, n_jobs=-1)
+gsmodel.fit(X, y)
+print "Best Model:"
+print gsmodel.best_estimator_
+print
+print "Best Parameters:"
+print gsmodel.best_params_
+print
+print "Best Score:"
+print gsmodel.best_score_
+
+
+# Exercises and ideas:
+# - benchmark your results against a dummy classifier
+# - choose a different datasets
+# - choose features with feature selection
+# - build a custom trasformer class
+# - build a custom classifier class
+# - choose an example from http://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_iris.html
+#   and try to reproduce it
+# - use feature extraction to classify unstructured data
+# - apply to a dataset of your choice
+
--- iris_starter_script.py
+++ iris_starter_script.py
@@ -1,31 +1,27 @@
-
 # coding: utf-8
+__author__ = "Francesco Mosconi"
+__copyright__ = "Copyright 2016, Data Weekends"
+__license__ = "MIT"
+__email__ = "info@dataweekends.com"
 
-# # Separating Flowers
-# This notebook explores a classic Machine Learning Dataset: the Iris flower dataset
-# 
-# ## Tutorial goals
-# 1. Explore the dataset
-# 2. Build a simple predictive modeling
-# 3. Iterate and improve your score
-# 
+"""
+Simple script detailing some of scikit-learn
+functions including:
+- train_test_split
+- model fitting and evaluation
+- decision tree classifier
+- confusion matrix
+"""
 
-# How to follow along:
-# 
-#     git clone https://github.com/dataweekends/pyladies_intro_to_data_science
-# 
-#     cd pyladies_intro_to_data_science
-#     
-#     ipython notebook
 
-# We start by importing the necessary libraries:
-
+# Import the necessary libraries:
 import pandas as pd
 import numpy as np
 
+# Read data from Files
 df = pd.read_csv('iris-2-classes.csv')
-
 df['target'] = df['iris_type'].map({'virginica': 1, 'versicolor': 0})
+
 
 # Define features (X) and target (y) variables
 X = df[['sepal_length_cm', 'sepal_width_cm',
@@ -34,31 +30,25 @@ y = df['target']
 
 
 # Initialize a decision tree model
-# hint: you can try changing the type of model or the model parameters
-
 from sklearn.tree import DecisionTreeClassifier
 model = DecisionTreeClassifier(max_depth = 5, random_state=0)
 
 #  Split the features and the target into a Train and a Test subsets.  
 #  Ratio should be 80/20
-
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, 
                             test_size = 0.3, random_state=0)
 
 
-
 # Train the model
 model.fit(X_train, y_train)
 
-
-# Calculate the model score
+# Calculate the model accuracy score
 my_score = model.score(X_test, y_test)
 
 print "\n"
 print "Using model: %s" % model
 print "Classification Score: %0.2f" % my_score
-
 
 # Print the confusion matrix for the decision tree model
 from sklearn.metrics import confusion_matrix
